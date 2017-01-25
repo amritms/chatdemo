cb081b6f927b4bb8172d3354557ed616f1a2226e
Amrit Man Shrestha <amritms@gmail.com>
2017-01-25 22:42:23 +0545

    initial commit with simple chat page

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Chat Room</title>
+  </head>
+  <body>
+
+<h1>Chat Room</h1>
+
+<div id="app">
+  <example></example>
+</div>
+
+  <script type="text/javascript" src="js/app.js"></script>
+
+  </body>
+</html>
